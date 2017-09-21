f236e645c945ff0616525885ae1e2333c2757ad5
Nelson Pereira Pinto Neto <neelsoonzd@hotmail.com>
2017-09-21 16:01:20 -0300

    Estudo de Objects

---
+++ Object/Funcionario.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object
+{
+    class Funcionario
+    {
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+
+        public override string ToString()
+        {
+            return "Funcionário: " + this.Nome + " - Salário: " + this.Salario;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Funcionario outro = (Funcionario)obj;
+            return this.Nome == outro.Nome;
+        }
+    }
+}
---
+++ Object/Teste.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Object
+{
+    class Teste
+    {
+        static void Main(string[] args)
+        {
+            Funcionario f1 = new Funcionario();
+
+            f1.Nome = "Abondanzieri";
+            f1.Salario = 2000;
+
+            Funcionario f2 = new Funcionario();
+
+            f2.Nome = "Claudio Nunes";
+            f2.Salario = 15000;
+
+            Console.WriteLine(f1 == f2);
+            Console.WriteLine(f1.Equals(f2));
+
+            Console.ReadKey();
+        }
+    }
+}
